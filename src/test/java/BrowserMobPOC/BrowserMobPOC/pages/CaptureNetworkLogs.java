77ba039f4838cc4d406dfd07b0e8ce65dae47a17
Ramit Dhamija <you@example.com>
2021-04-03 18:48:37 +0530

    added chrome options for jenkins build

--- src/test/java/BrowserMobPOC/BrowserMobPOC/pages/CaptureNetworkLogs.java
+++ src/test/java/BrowserMobPOC/BrowserMobPOC/pages/CaptureNetworkLogs.java
@@ -43,6 +43,10 @@ public class CaptureNetworkLogs {
 		   
 		    DesiredCapabilities capabilities = new DesiredCapabilities();
 		    ChromeOptions options = new ChromeOptions();
+		    options.addArguments("--no-sandbox");
+		    options.addArguments("--headless"); //!!!should be enabled for Jenkins
+		    options.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
+		    options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
 		    options.addArguments("--ignore-certificate-errors");
 		    capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
 		    capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS,true);
